1edee8dbc0fd1974e2a75fd981e6c84db0d73f96
angelilucila <73862577+angelilucila@users.noreply.github.com>
2021-02-23 21:53:34 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_craigslist
+import scrape_mars
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-
-
 @app.route("/")
-def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", news=news,images=images,facts=facts,hemispheres=hemispheres )
+def scrape():
+    return render_template("index.html", title= mars.news_title, news_p = mars.news_p ,jpl_url = mars.featured_image_url, hem_img = mars.image_url, description=description, image_title = mars.image_title, table = mars.html_table)
 
-
-@app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_mars.scrape()
-    listings.update({}, listings_data, upsert=True)
-    return redirect("/", code=302)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
